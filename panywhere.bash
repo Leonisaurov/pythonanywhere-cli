7018f7757060478c31f7a6c97980cf6849a32046
Leonisaurov <leonisaurov@gmail.com>
2025-06-04 20:12:43 -0600

    Initial Commit: Functional Tool

---
+++ panywhere.bash
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "You need to specify a action."
+    echo "Usage: $0 <cpu|upload|get|reload|upload-hook>"
+    exit -1
+fi
+
+[ ! -f ".env" ] || export $(grep -v '^#' '.env' | xargs)
+
+username="$PYTHONANYWHERE_USER"
+api_key="$PYTHONANYWHERE_API_TOKEN"
+auth_header="Authorization: Token $api_key"
+
+host="${PYTHONANYWHERE_HOST:-https://www.pythonanywhere.com/}"
+api_host=$host"api"
+webhost=$username".pythonanywhere.com"
+
+site_path="${PYTHONANYWHERE_SITE_PATH:-mysite}"
+base_path="/home/$username/$site_path"
+
+cpu_info() {
+    res=$(curl -s -X GET $api_host"/v0/user/$username/cpu/" \
+        -H "$auth_header")
+    echo $res | jq -r 'to_entries | .[] | "\(.key) = \(.value)"'
+}
+
+get_file() {
+    if [ $# -lt 2 ]; then
+        echo "You need to specify a file path."
+        echo "Usage: $0 $1 <fileName>"
+        exit -2
+    fi
+
+    file_path=$2
+    res=$(curl -s -X GET $api_host"/v0/user/$username/files/path/$base_path/$file_path" \
+        -H "$auth_header")
+    if [[ "$res" == "{"* ]]; then
+        echo "$res" | jq -r 'if has("detail") then .detail else to_entries | .[] | if .value.type == "directory" then "󰉋 \(.key)" else "󰈔 \(.key)" end end'
+    else
+        echo "$res"
+        echo -n "$res" > "$file_path"
+    fi
+}
+
+upload_file() {
+    if [ $# -lt 2 ]; then
+        echo "You need to specify a file path."
+        echo "Usage: $0 $1 <fileName>"
+        exit -2
+    fi
+
+    file_path=$2
+    if [[ ! -e "$file_path" ]]; then
+        echo "The file '$file_path' doesn't exist."
+        exit -3
+    fi
+
+    res=$(curl -s -o /dev/null -w "%{http_code}" -X POST $api_host"/v0/user/$username/files/path/$base_path/$file_path" \
+        -H "$auth_header" \
+        -F "content=@"$file_path)
+    if [ "$res" -eq 200 ]; then
+        echo "File '$file_path' updated."
+    else
+        echo "File '$file_path' created."
+    fi
+}
+
+reload_webpage() {
+    res=$(curl -s -X POST $api_host"/v0/user/$username/webapps/$webhost/reload/" \
+        -H "$auth_header")
+    echo $res | jq -r 'if has("status") then "Status: \(.status)" else "Unknown response:\n\(.)" end'
+}
+
+set_pre_commit() {
+    hook_code="#!/bin/bash
+staged_files=\$(git diff --cached --name-only)
+
+for file in \$staged_files; do
+    $0 upload "\$file"
+done"
+    mkdir -p .git/hooks
+    echo "$hook_code" >> ".git/hooks/pre-commit"
+    chmod +x ".git/hooks/pre-commit"
+}
+
+case "$1" in
+    cpu)
+        cpu_info
+        ;;
+    get)
+        get_file $@
+        ;;
+    upload)
+        upload_file $@
+        ;;
+    upload-hook)
+        set_pre_commit $@
+        ;;
+    reload)
+        reload_webpage $@
+        ;;
+    *)
+        echo "Unknown action '$1'"
+        ;;
+esac
---
+++ panywhere.py
@@ -0,0 +1,138 @@
+#!/bin/python3
+import sys
+import requests
+
+from dotenv import load_dotenv
+import os
+
+argv = sys.argv
+if len(argv) < 2:
+    print('You need to specify a action.')
+    print(f'usage: {argv[0]} <cpu|upload|get|reload|upload-hook>')
+    sys.exit(-1)
+
+load_dotenv()
+
+username = os.getenv('PYTHONANYWHERE_USER')
+token = os.getenv('PYTHONANYWHERE_API_TOKEN', '')
+host = os.getenv('PYTHONANYWHERE_HOST', 'https://www.pythonanywhere.com/') + 'api'
+webhost = os.getenv('PYTHONANYWHERE_WEBHOST', username + '.pythonanywhere.com')
+
+site_path = os.getenv('PYTHONANYWHERE_SITE_PATH', 'mysite')
+base_server_file = f'/home/{username}/{site_path}'
+
+def cpu_info():
+    url = host + '/v0/user/{username}/cpu/'.format(username=username) 
+    response = requests.get(
+        url,
+        headers={'Authorization': 'Token {token}'.format(token=token)}
+    )
+    if response.status_code == 200:
+        print('CPU quota info:')
+        data = response.json()
+        for key in data:
+            print(f"{key}: {data[key]}")
+    else:
+        print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
+def upload_file():
+    if len(sys.argv) != 3:
+        print('You need to specify a file path')
+        print(f'Usage: {sys.argv[0]} <fileName>')
+        sys.exit(-1)
+
+    file_path = sys.argv[2]
+
+    url = host + '/v0/user/{username}/files/path/{base_path}/{path}'.format(
+            username=username, 
+            base_path=base_server_file,
+            path=file_path) 
+
+    with open(file_path, 'rb') as f:
+        files = {
+                "content": f,
+        }
+
+        res = requests.post(url,
+                            headers={'Authorization': 'Token {token}'.format(token=token)},
+                            files=files)
+    if res.status_code == 200:
+        print(f"File '{file_path}' updated.")
+    else:
+        print(f"File '{file_path}' created.")
+
+def get_file():
+    if len(argv) != 3:
+        print('You need to specify a file path')
+        print(f'Usage: {argv[0]} {argv[1]} <fileName>')
+        sys.exit(-1)
+
+
+    file_path = sys.argv[2]
+
+    url = host + '/v0/user/{username}/files/path/{base_path}/{path}'.format(
+            username=username, 
+            base_path=base_server_file,
+            path=file_path) 
+
+    res = requests.get(url,
+                        headers={'Authorization': 'Token {token}'.format(token=token)})
+    print("Código de estado:", res.status_code)
+
+    txt = res.text
+    if not txt.startswith('{'):
+        dir_i = file_path.rfind('/')
+        if dir_i != -1:
+            dir = file_path[:dir_i]
+            os.makedirs(dir, exist_ok=True)
+        with open(file_path, 'w') as file:
+            file.write(txt)
+            print(res.text)
+    else:
+        files = res.json()
+        if 'detail' in files:
+            print(files['detail'])
+        else:
+            for file in files:
+                d = files[file]['type'] == 'directory'
+
+                if d:
+                    print('󰉋 ', end='')
+                else:
+                    print('󰈔 ', end='')
+                print(file)
+
+def reload_page():
+    url = host + '/v0/user/{username}/webapps/{domain_name}/reload/'.format(
+        username=username,
+        domain_name=webhost
+        )
+    res = requests.post(url,
+        headers={'Authorization': 'Token {token}'.format(token=token)}).json()
+    if 'status' in res:
+        print(f"Status: {res['status']}")
+
+def set_pre_commit():
+    hook_code = f"""#!/bin/bash
+staged_files=$(git diff --cached --name-only)
+
+for file in $staged_files; do
+    {argv[0]} upload "$file"
+done"""
+    os.makedirs('.git/hooks')
+    with open('.git/hooks/pre-commit', 'a') as file:
+        file.write(hook_code)
+
+cmd = argv[1]
+if cmd == 'cpu':
+    cpu_info()
+elif cmd == 'get':
+    get_file()
+elif cmd == 'upload':
+    upload_file()
+elif cmd == 'reload':
+    reload_page()
+elif cmd == 'upload-hook':
+    set_pre_commit()
+else:
+    print(f"Uknown action '{cmd}'")
